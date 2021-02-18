878e6ea3c6a7cdd7b3b23cff670de70114c6a31b
Leon <l.botros@cs.ru.nl>
2021-02-18 14:32:27 +0100

    refactor config validation into config directory, add config to request

--- backend/serverA/server.js
+++ backend/serverA/server.js
@@ -1,35 +1,16 @@
 const express = require('express')
 const cookieSession = require('cookie-session')
+
 const { createProxyMiddleware: proxy } = require('http-proxy-middleware')
 const bodyParser = require('body-parser')
-
 const sessionPtrMiddleware = require('./middlewares/sessionpointer')
-const { networkInterfaces } = require('os');
-
-var conf = require('./config/conf.json')
-
-if (!("external_url" in conf)) {
-	console.log("no 'external_url' set in config, resolving external ip address (assuming HTTP in dev mode)");
-	const nets = networkInterfaces();
-	for (const name of Object.keys(nets)) {
-			for (const net of nets[name]) {
-					// Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-					if (net.family === 'IPv4' && !net.internal) {
-						conf.external_url = "http://" + net.address + ":" + conf.port;
-						if (!("vote_url" in conf)) {
-							conf.vote_url = "http://" + net.address + ":" + conf.vote_port;
-						}
-						break;
-					}
-			}
-	}
-}
 
 const admin = require('./routes/admin')
 const votingcard = require('./routes/votingcard')
 const election = require('./routes/election')
 
 const db = require('./db/database')
+const conf = require('./config/config')
 
 const app = express()
 
@@ -40,9 +21,11 @@ const app = express()
 // Proxy IRMA app traffic to IRMA server
 app.use('/irma', proxy({ target: `${conf.irma.url}`, changeOrigin: true }))
 
-// Make the database globally accesible
+// Make the database and the configuration globally accesible
+// So we don't need to import those in every route handler for example.
 app.use(function (req, _, next) {
   req.db = db
+  req.conf = conf
   next()
 })
 
